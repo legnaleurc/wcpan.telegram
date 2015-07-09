665a82203cbfb5c5fb76b9e952d02414531c9a95
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-07-09 12:31:35 +0800

    Update metadata.

--- setup.py
+++ setup.py
@@ -31,16 +31,15 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
 
-    keywords='telegram tornado',
+    keywords='telegram bot tornado',
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
