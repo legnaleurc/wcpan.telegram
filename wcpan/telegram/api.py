17c17ba460a4b152a23ca384c0739b0d968d1d1b
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-12-08 16:52:18 -1000

    update

--- wcpan/telegram/api.py
+++ wcpan/telegram/api.py
@@ -187,12 +187,18 @@ class BotClient(object):
 
         return types.Message(data)
 
-    async def send_sticker(self, chat_id, sticker, reply_to_message_id=None,
-                           reply_markup=None):
+    async def send_sticker(self, chat_id: Union[int, str],
+                           sticker: Union[types.InputFile, str],
+                           disable_notification: bool = None,
+                           reply_to_message_id: int = None,
+                           reply_markup: ReplyMarkup = None
+                           ) -> Awaitable[types.Message]:
         args = {
             'chat_id': chat_id,
             'sticker': sticker,
         }
+        if disable_notification is not None:
+            args['disable_notification'] = disable_notification
         if reply_to_message_id is not None:
             args['reply_to_message_id'] = reply_to_message_id
         if reply_markup is not None:
